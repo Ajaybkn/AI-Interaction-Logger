58786d11d2ecf630cd87dd924a6bac684ea9562d
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-08-25 12:53:36 +0530

    update in routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,9 +30,9 @@ app.use("/api/lists", listRoutes);
 app.use("/api/cards", cardRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get(/^(?!\/api).*/, (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 // Error Handling
 app.use(notFound);
 app.use(errorHandler);
